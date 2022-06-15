ea0afb64a4fde28c69d590337168b8712d04b620
Isabela de Aro <euisabeladearo@gmail.com>
2022-06-15 14:41:59 -0300

    feat: add migrations and models for categories and postCategory

---
+++ src/database/migrations/20220615151945-create-categories.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const Categories = queryInterface.createTable("Categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+
+    return Categories;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable("Categories"),
+};
