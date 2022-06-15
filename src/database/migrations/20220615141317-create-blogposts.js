6f7f343c107503ac4b31db3497e4355305a84c45
Isabela de Aro <euisabeladearo@gmail.com>
2022-06-15 14:40:32 -0300

    feat: add migrations and models for user and blogPost

---
+++ src/database/migrations/20220615141317-create-blogposts.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const BlogPosts = await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        type: Sequelize.INTEGER,
+        
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    return BlogPosts;
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('BlogPosts');
+  },
+};
