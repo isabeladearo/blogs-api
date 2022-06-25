4f1c95d3c8a2611eeca890f0cbe4a9e42f949a2b
Isabela de Aro <euisabeladearo@gmail.com>
2022-06-24 22:28:29 -0300

    refactoring: endpoint to create blogPost

--- src/middlewares/validateCreatePost.js
+++ src/middlewares/validateCreatePost.js
@@ -1,45 +1,27 @@
 const Joi = require('joi');
 
-const { Category } = require('../database/models');
-
 const userSchema = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
-  categoryIds: Joi.array().required(),
+  categoryIds: Joi.array().min(1).required(),
 });
 
 const handleError = (error) => {
   const { type } = error.details[0];
 
-  if (['any.required', 'string.empty', 'array.empty'].includes(type)) {
+  const MISSING_TYPES = ['any.required', 'string.empty', 'array.empty', 'array.min'];
+
+  if (MISSING_TYPES.includes(type)) {
     return 'Some required fields are missing';
   }
 
   return error.message;
 };
 
-const checkCategoriesIds = (array) => Promise.all(
-  array.map(async (id) => {
-    const idFound = await Category.findOne({ where: { id } });
-    if (!idFound) return false;
-    return true;
-  }),
-);
-
 module.exports = async (req, res, next) => {
   const { error } = userSchema.validate(req.body);
 
-  if (error) {
-    return res.status(400).json({ message: handleError(error) });
-  }
+  if (!error) return next();
 
-  if (req.body.categoryIds) {
-    const allCategoriesExists = await checkCategoriesIds(req.body.categoryIds);
-
-    if (allCategoriesExists.includes(false)) {
-      return res.status(400).json({ message: '"categoryIds" not found' });
-    }
-
-    return next();
-  }
+  return res.status(400).json({ message: handleError(error) });
 };
